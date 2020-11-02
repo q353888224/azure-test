a5e83bc3491df6849da3d1743f8ad2af8cadb99c
张帅 <shuai.zhang@monstar-lab-cn.com>
2020-11-02 16:26:56 +0800

    first commit

---
+++ src/main/java/com/wscq/bootbasesimple/BootBaseSimpleApplication.java
@@ -0,0 +1,15 @@
+package com.wscq.bootbasesimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangshuai
+ */
+@SpringBootApplication
+public class BootBaseSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootBaseSimpleApplication.class, args);
+    }
+}
---
+++ src/main/java/com/wscq/bootbasesimple/web/controller/DemoController.java
@@ -0,0 +1,11 @@
+package com.wscq.bootbasesimple.web.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhangshuai
+ */
+@RestController
+public class DemoController {
+
+}
